9a5d671484292d54e0466845b38830865706cfad
tatianat0 <t.titovaniko00@gmail.com>
2026-05-06 17:42:31 +0600

    homework2_m2 added.

---
+++ HomeWork/homework2_m2.py
@@ -0,0 +1,44 @@
+# Домашнее задание № 2
+from HomeWork.homework1_m2 import Hero
+
+
+class Warrior(Hero):
+    def __init__(self, name, level, health, strength, stamina):
+        super().__init__(name, level, health, strength)
+        self.stamina = stamina
+
+    def attack(self):
+        return f'{self.name} - Воин атакует мечом!'
+
+class Mage(Hero):
+    def __init__(self, name, level, health, strength, mana):
+        super().__init__(name, level, health, strength)
+        self.mana = mana
+
+    def attack(self):
+        return f'{self.name} - Маг кастует заклинание!'
+
+class Assassin(Hero):
+    def __init__(self, name, level, health, strength, stealth):
+        super().__init__(name, level, health, strength)
+        self.stealth = stealth
+
+    def attack(self):
+        return f'{self.name} - Ассасин атакует из - под тишка!'
+
+
+din = Warrior("Дин", 2, 8, 90, 30)
+kass = Mage("Касс", 3, 5, 100, 25)
+lilit = Assassin("Лилит", 1, 3, 80, 20)
+
+print(din.greet())
+print(din.attack())
+print(din.rest(1))
+
+print(kass.greet())
+print(kass.attack())
+print(kass.rest(2))
+
+print(lilit.greet())
+print(lilit.attack())
+print(lilit.rest(2))
---
+++ lessons/lesson2_m2.py
@@ -0,0 +1,72 @@
+"""Урок №2 Принципы ООП -  Наследование, Полиморфизм."""
+
+# Родительский|Супер класс
+class Hero:
+    def __init__(self, name, hp, lvl):
+        self.name = name
+        self.hp = hp
+        self.lvl = lvl
+
+    def action(self):
+        print(f"{self.name} base action")
+
+
+# Дочерний класс
+class MageHero(Hero):
+
+    def __init__(self, name, hp, lvl, mp):
+        super().__init__(name, hp, lvl)
+        self.mp = mp
+
+    def cast_spell(self):
+        print('Огненный щар!!')
+
+    def action(self):
+        print(f"Это новый метод дочернего класса!! {self.name}")
+
+
+kirito = Hero("KIrito", 100, 10)
+asuna = MageHero("Asuna", 1000, 100, 100)
+
+# kirito.action()
+# asuna.action()
+
+class Fly:
+    def action(self):
+        print("Fly")
+
+class Swim:
+    def action(self):
+        print("Swim")
+
+class Animal(Fly, Swim):
+    # def action(self):
+    #     print("Step")
+    pass
+
+donald_duck = Animal()
+
+# donald_duck.action()
+
+
+
+
+class A:
+    def action(self):
+        print("A")
+class B(A):
+    def action(self):
+        super().action()
+        print('B')
+class C(A):
+    def action(self):
+        super().action()
+        print('C')
+class D(C, B):
+    def action(self):
+        super().action()
+        print('D')
+
+test_obj = D()
+
+print(D.__mro__)
